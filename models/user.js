d68873cf56a11b9511b0dedf0ff88812a4355845
MACOS <macos@MACOSs-MacBook-Pro.local>
2022-12-20 23:24:17 +0700

    done

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  constructor(username, email, cart) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = User;
